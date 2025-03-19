8c2c9a6c1865813eee5e4e2957f2599bc9886e9e
unknown <clement.slvdr@gmail.com>
2025-03-19 21:41:35 +0100

    init!: creating project game data api

---
+++ src/main/java/com/ismokewindow/api/gamedata/GameDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.ismokewindow.api.gamedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismokewindow/api/gamedata/GameDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismokewindow.api.gamedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
